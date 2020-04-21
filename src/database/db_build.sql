6249bf51574acb0a5d95cfc4f2d1fb7b8830ef31
moris <moris.rafol@gmail.com>
2020-04-21 19:25:10 +0300

    merge branches

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,32 +1,33 @@
 BEGIN;
- 
-DROP TABLE IF EXISTS sitters CASCADE;
+
+DROP TABLE IF EXISTS sitters,reservations CASCADE;
 
 CREATE TABLE sitters(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL, 
-    starting_hour TIME NOT NULL,
-    end_hour TIME NOT NULL,
-    cost INTEGER NOT NULL,
-    available_date DATE NOT NULL
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(100) NOT NULL,
+                        starting_hour TIME(HH:MM) NOT NULL,
+                        end_hour TIME NOT NULL,
+                        cost INTEGER NOT NULL
 );
 
-CREATE TABLE reservation(
-    id SERIAL PRIMARY KEY, 
-    reservant_full_name VARCHAR(100) NOT NULL,
-    reservant_phone VARCHAR(15) NOT NULL,
-    starting_hour TIME NOT NULL,
-    end_hour TIME NOT NULL,
-    sitter_id INTEGER REFERENCES sitters(id),
-    is_reserved BOOLEAN
+CREATE TABLE reservations(
+                            id SERIAL PRIMARY KEY,
+                            reservant_full_name VARCHAR(100) NOT NULL,
+                            reservant_phone VARCHAR(15) NOT NULL,
+                            starting_hour TIME NOT NULL,
+                            end_hour TIME NOT NULL,
+                            sitter_id INTEGER ,
+                            is_reserved BOOLEAN DEFAULT true
+--                             FOREIGN KEY(sitter_id) REFERENCES  sitters(id)
 );
 
 
-INSERT INTO sitters (name,starting_hour, end_hour, cost) 
-VALUES('Morad Abed', '11:30', '13:00', '11:00');
+INSERT INTO sitters (name,starting_hour, end_hour, cost)
+VALUES('Morad Abed', '11:30', '13:00', '50' );
 
 
-INSERT INTO reservation (reservant_full_name, reservant_phone, starting_hour, end_hour,sitter_id) 
-VALUES('Marwan Rizik', '050111111', '11:30', '13:00'); 
+INSERT INTO reservations (reservant_full_name, reservant_phone, starting_hour, end_hour,sitter_id)
+VALUES('Marwan Rizik', '050111111', '11:30', '13:00',0);
+
 
 COMMIT;
--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,12 +1,16 @@
 const { Pool } = require('pg');
-
 require('dotenv').config();
 
-const connectionString = process.env.DB_URL
+
+let connectionString = process.env.DB_URL;
+if(process.env.NODE_ENV === "test")
+    connectionString = process.env.DATABASE_TEST_URL;
+
+
 
 if(!connectionString)
 throw new Error('Env variable DB_URL must be set');
 
-module.exports = new Pool(
-    connectionString
-)
+
+
+module.exports = new Pool(connectionString)
