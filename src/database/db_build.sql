c8df32f39f1e38e1816147b0497ef1fc434d9b30
Morad Abed <morad890@gmail.com>
2020-04-21 17:44:33 +0300

    creating and linking the tables

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,12 +1,28 @@
 BEGIN;
 
-CREATE TABLE reservation(
+CREATE TABLE sitters(
     id SERIAL PRIMARY KEY,
-    sitter_id INTEGER REFERENCES sitters(id), 
+    sitter_name VARCHAR(100) NOT NULL, 
+    starting_hour TIME NOT NULL,
+    end_hour TIME NOT NULL,
+    cost INTEGER NOT NULL
+);
+
+CREATE TABLE reservation(
+    id SERIAL PRIMARY KEY, 
     reservant_full_name VARCHAR(100) NOT NULL,
-    reservan_phone INTEGER(15) NOT NULL,
-    starting_hour INTEGER ,
-    end_hour INTEGER
-)
+    reservant_phone VARCHAR(15) NOT NULL,
+    starting_hour TIME NOT NULL,
+    end_hour TIME NOT NULL,
+    sitter_id INTEGER REFERENCES sitters(id)
+);
+
+
+INSERT INTO sitters (sitter_name,starting_hour, end_hour, cost) 
+VALUES('Morad Abed', '11:30', '13:00', 100);
+
+
+INSERT INTO reservation (reservant_full_name, reservant_phone, starting_hour, end_hour,sitter_id) 
+VALUES('Marwan Rizik', '050111111', '11:30', '13:00'); 
 
 COMMIT;
