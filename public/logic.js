dcd3752de5d7f13e21cbc7190831e9ee761174bf
moris <moris.rafol@gmail.com>
2020-04-22 15:57:19 +0300

    redesign logic.js file

--- public/logic.js
+++ public/logic.js
@@ -1,75 +1,82 @@
 
 
-function getAllReservations(cb) {
-    fetch('/reservations')
-        .then(resp => resp.json())
-        .then(obj => cb(null, obj))
-        .catch(err => cb(err));
+const logic =
+    {
 
-}
+    getAllReservations:
+        function (cb) {
+            fetch('/reservations')
+                .then(resp => resp.json())
+                .then(obj => cb(null, obj))
+                .catch(err => cb(err));
 
-function getReservations(count, offset, cb) {
+        },
 
-    fetch(`/reservations?count=${count}&offset=${offset}`)
-        .then(resp => resp.json())
-        .then(obj => cb(null, obj))
-        .catch(err => cb(err));
+    getReservations:
+        function (count, offset, cb) {
 
-}
+            fetch(`/reservations?count=${count}&offset=${offset}`)
+                .then(resp => resp.json())
+                .then(obj => cb(null, obj))
+                .catch(err => cb(err));
 
-function addReservation(name, phone, startingHour, endHour, sitterId, cb) {
+        },
 
-
-    let dataObj = {
-        name,
-        phone,
-        startingHr: startingHour,
-        endHr: endHour,
-        sitterId
-    };
+    addReservation:
+        function (name, phone, startingHour, endHour, sitterId, cb) {
 
 
-    fetch(`/reservations`, {
-        method: "post",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(dataObj)
-    })
-        .catch(err => cb(err));
-
-}
-
-
-//sitters fetching
-function getAllSitters(cb) {
-    fetch('/sitters')
-        .then(res => res.json())
-        .then(obj => cb(null, obj))
-        .catch(err => cb(err));
-}
-
-function getSitters(count, offset, cb) {
-    fetch(`/sitters?count=${count}&offset=${offset}`)
-        .then(res => res.json())
-        .then(obj => cb(null, obj))
-        .catch(err => cb(err));
-}
+            let dataObj = {
+                name,
+                phone,
+                startingHr: startingHour,
+                endHr: endHour,
+                sitterId
+            };
 
 
-function addSitters(name, startingHour, endHour, cost) {
+            fetch(`/reservations`, {
+                method: "post",
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify(dataObj)
+            })
+                .catch(err => cb(err));
 
-    let sitterObj = {
-        name,
-        startingHr: startingHour,
-        endHr: endHour,
-        cost
-    }
+        },
 
-    fetch('/sitters', {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(sitterObj)
-    })
-        .catch(err => cb(err));
+    getAllSitters:
+        function (cb) {
+            fetch('/sitters')
+                .then(res => res.json())
+                .then(obj => cb(null, obj))
+                .catch(err => cb(err));
+        },
+
+    getSitters:
+        function (count, offset, cb) {
+            fetch(`/sitters?count=${count}&offset=${offset}`)
+                .then(res => res.json())
+                .then(obj => cb(null, obj))
+                .catch(err => cb(err));
+        },
+
+    addSitters:
+        function addSitters(name, startingHour, endHour, cost, cb) {
+
+            let sitterObj = {
+                name,
+                startingHr: startingHour,
+                endHr: endHour,
+                cost
+            }
+
+            fetch('/sitters', {
+                method: "POST",
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify(sitterObj)
+            })
+                .catch(err => cb(err));
+        },
 }
 
 export default logic;
